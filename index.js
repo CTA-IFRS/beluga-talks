9ceba8d68da33aa4aac128ccdff96227ed3e88ab
EllisPagel <ellispagel.silva@gmail.com>
2020-03-09 09:39:32 -0200

    Teste

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // verifica se o navegador suporta service workers
-		if (navigator.serviceWorker) {
-            // Registra o service worker
-            navigator.serviceWorker.register("./sw.js")
-                .then(function (registration) {
+    if (navigator.serviceWorker) {
+        // Registra o service worker
+        navigator.serviceWorkerRegistration.register("./sw.js")
+            .then(function (registration) {
                 console.info("Service Worker registration successful with scope: ", registration.scope);
-                })
-                .catch(function (err) {
+            })
+            .catch(function (err) {
                 // Log do erro caso não consiga registrar o service worker
                 console.error("Service Worker registration failed: ", err);
-                });
-            }
+            });
+        }
