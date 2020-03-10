5d8a4f2afd0fcf666e81f2e6f2d8c18fd4fc62c9
CamilleBorges <camilleborges167@gmail.com>
2020-03-10 09:39:50 -0400

    Correção do request service worker de novo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // verifica se o navegador suporta service workers
   if ('serviceWorker' in navigator) {
     // Registre o service worker
-     navigator.serviceWorker.register ('/serviceWorker.js')
+     navigator.serviceWorker.register ('./serviceWorker.js')
       .then (function (registration) {
       console.info ("Registro do Service Worker bem-sucedido com escopo:", registration.scope);
       })
@@ -10,23 +10,3 @@
       console.error( 'Registro do ServiceWorker falhou:' ,  err );
       });
   }
-
-  //function to perform HTTP request
-  var get = function(url){
-    return new Promise(function(resolve, reject){
-      var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function(){
-        if(xhr.readyState === XMLHttpRequest.DONE){
-          if(xhr.status === 200){
-            var result = xhr.responseText
-            result = JSON.parse(result);
-          }else{
-            reject(xhr);
-          }
-        }
-      };
-
-      xhr.open("GET", url, true);
-      xhr.send();
-    });
-  };
