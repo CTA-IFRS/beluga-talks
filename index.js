14ade4e5118cf675e686cebf73c9c2161ada8f43
CamilleBorges <camilleborges167@gmail.com>
2020-03-10 08:38:29 -0400

    teste

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // verifica se o navegador suporta service workers
-  if (navigator.serviceWorker) {
+  if ('serviceWorker' in navigator) {
+
+    console.log("está aqui");
     // Registre o service worker
-    navigator.serviceWorker.register ("./sw.js")
-    .then (function (registration) {
-    console.info ("Registro do Service Worker bem-sucedido com escopo:", registration.scope);
-    })
-    .catch (function (err) {
-    // Log do erro não consiga registrar o service worker
-    console.error( 'Registro do ServiceWorker falhou:' ,  err );
-    });
+     navigator.serviceWorker
+      .register ('./serviceWorker.js', { scope: './' })
+      .then (function (registration) {
+      console.info ("Registro do Service Worker bem-sucedido com escopo:", registration.scope);
+      })
+      .catch (function (err) {
+      // Log do erro não consiga registrar o service worker
+      console.error( 'Registro do ServiceWorker falhou:' ,  err );
+      });
   }
---
+++ serviceWorker.js
@@ -0,0 +1,59 @@
+// Criando um nome para o arquivo de cache
+const staticCache = "prancha_alfanumerica_2020_03_09_10_39"; //ano_mes_dia_hora_min
+
+//Install
+// Lista de arquivos que devem ser cacheados
+const files = [
+    '/',
+    '/LICENSE',
+    '/estilo.css',
+    '/manifest.json',
+    '/imagens/Icon10.png',
+  ];
+
+// Faz cache dos arquivos ao instalar 
+this.addEventListener("install", event => {
+    this.skipWaiting();
+  
+    event.waitUntil(
+      caches.open(staticCache)
+        .then(cache => {
+          return cache.addAll(files);
+      })
+    )
+});
+
+//Activate
+// Limpa o cache antigo 
+this.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames
+            .filter(cacheName => (cacheName.startsWith('prancha_alfanumerica_')))
+            .filter(cacheName => (cacheName !== staticCache))
+            .map(cacheName => caches.delete(cacheName))
+        );
+      })
+    );
+  });
+
+ //Fetch
+ // Reponde o request direto do cache
+this.addEventListener("fetch", event => {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          // Retorna o cache
+          if (response) {
+            return response;
+          }
+          // Faz a requisição  
+          return fetch(event.request);
+        })
+        .catch(() => {
+          // Mostra uma página de offline
+          return caches.match('/index.html');
+        })
+    )
+  }); 
