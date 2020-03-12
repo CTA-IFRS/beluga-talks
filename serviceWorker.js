01a15e423b96ba97eeb12cc67c4c0946a08eeb6f
EllisPagel <ellispagel.silva@gmail.com>
2020-03-12 10:13:55 -0200

    Mudança na start_url

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,5 @@
 // Criando um nome para o arquivo de cache
-var staticCache = "prancha_alfanumerica_2020_03_12_09_35"; //ano_mes_dia_hora_min
+var staticCache = "prancha_alfanumerica_2020_03_12_10_13"; //ano_mes_dia_hora_min
 
 console.log("Inicio do cache");
 
